14a93a1fb7f3949b3dde5a201d8313dbcf9b1079
Rasinhubs <raguwork06@gmail.com>
2025-08-08 17:04:21 +0530

    Add files via upload
    Added child project using java to practice jenkins with Ngrok

---
+++ ChildProject.java
@@ -0,0 +1,6 @@
+public class ChildProject {
+     public static void main(String[] args) {
+          System.out.println("Hai I am your child projecct");
+          System.out.println("Thanks for reaching me! I always run after you.");
+     }
+}
