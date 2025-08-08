fb1d1e9daf0d25a2e8e35fdc210b4a5904d03e1b
Rasinhubs <raguwork06@gmail.com>
2025-08-08 17:42:16 +0530

    Update ChildProject.java
    Added new line to check when giving commit in jenkins.

--- ChildProject.java
+++ ChildProject.java
@@ -2,5 +2,7 @@ public class ChildProject {
      public static void main(String[] args) {
           System.out.println("Hai I am your child projecct");
           System.out.println("Thanks for reaching me! I always run after you.");
+           System.out.println("Added new line.");
      }
+
 }
